65e1494cf70196a03232a21e99d0a137d3b63baa
shumovsky <d.shumovsky@bk.ru>
2020-01-09 20:08:01 +0300

    master

--- js/main.js
+++ js/main.js
@@ -21,3 +21,15 @@ $('.slade-news').slick({
       wrap: true,
     });
   });
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute('href')
+      document.querySelector ('' + blockID).scrollIntoView({ 
+        behavior: "smooth",
+        block: "start"
+      })
+    })
+  }
